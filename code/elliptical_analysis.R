2c63e42ef877dd068cfd99e6789dfbc1db1b94fa
Kit Kemper <cgkemper@gmail.com>
2019-04-30 11:48:55 -0400

    Calculated mnean heart rate for each entry.

--- code/elliptical_analysis.R
+++ code/elliptical_analysis.R
@@ -1,10 +1,22 @@
 library(airtabler)
 library(dplyr)
+library(ggplot2)
 
-Sys.setenv("AIRTABLE_API_KEY"="<keypchGI5tV3dEicT")
+## Load Airtable data into DataFrame and query top rows
+Sys.setenv("AIRTABLE_API_KEY"="keypchGI5tV3dEicT")
 airtable <- airtabler::airtable("app3gYedYvqJRvElw", "Elliptical Workouts") 
-
 airtable <- airtable$'Elliptical Workouts'$select_all()
+head(airtable)
 
-airtable
+## Line Graph of Various Heart Rates vs. Time
+airtable <- airtable %>% mutate(Date = as.Date(Date))
+airtable <- na.omit(airtable)
+heart_rate <- c(airtable$`Elliptical Heart Rate (bpm)`, airtable$`Runkeeper Heart Rate (bpm)`, airtable$`AppleWatch Heart Rate (bpm)`)
+plot(airtable$Date, heart_rate, type="l")
 
+## Calculate mean heart rate
+airtable$'Mean Heart Rate (bpm)' <- (as.integer(airtable$`Elliptical Heart Rate (bpm)`) + as.integer(airtable$`Runkeeper Heart Rate (bpm)`) + as.integer(airtable$`AppleWatch Heart Rate (bpm)`))/3
+summary(airtable)
+head(airtable)
+x <- as.integer(c(airtable$`Elliptical Heart Rate (bpm)`, airtable$`Runkeeper Heart Rate (bpm)`, airtable$`AppleWatch Heart Rate (bpm)`, airtable$`Mean Heart Rate (bpm)`))
+plot(as.integer(airtable$`Pace (min/mile)`), as.integer(airtable$`Mean Heart Rate (bpm)`))
