814b3631dd35deea4a9e889bc6b1c6f4cab0f37e
Kit Kemper <cgkemper@gmail.com>
2019-05-01 12:46:41 -0400

    Replaced all table references with variables to clean up the code.  Fixed issue NaN appearing in Runkeeper HR. Fixed ANOVA. Swithched pace vs. mhr to scatter plot. Did pairs analysis between all factors. Binned Miles.

--- code/elliptical_analysis.R
+++ code/elliptical_analysis.R
@@ -8,85 +8,111 @@
   Sys.setenv("AIRTABLE_API_KEY"="keypchGI5tV3dEicT")
   airtable <- airtabler::airtable("app3gYedYvqJRvElw", "Elliptical Workouts") 
   airtable <- airtable$'Elliptical Workouts'$select_all()
+  airtable <- na.omit(airtable)
+  
+  # Set  Variables
+  level <- airtable$Level
+  time <- airtable$`Time (Minutes)`
+  calories <- airtable$Calories
+  miles <- airtable$`Distance (Miles)`
+  distanceClimbed <- airtable$`Distance Climbed`
+  speed <- airtable$`Speed(mph)`
+  pace <- airtable$`Pace (min/mile)`
+  ehr <- airtable$`Elliptical Heart Rate (bpm)`
+  rhr <- airtable$`Runkeeper Heart Rate (bpm)`
+  awhr <- airtable$`AppleWatch Heart Rate (bpm)`
   
  # Convert columns into numerals
-  airtable$Level <- as.integer(airtable$Level)
-  airtable$`Time (Minutes)`<- as.integer(airtable$`Time (Minutes)`)
-  airtable$Calories <- as.integer(airtable$Calories)
-  airtable$`Distance (Miles)` <- as.integer(airtable$`Distance (Miles)`)
-  airtable$`Distance Climbed` <- as.integer(airtable$`Distance Climbed`)
-  airtable$`Speed(mph)` <- as.numeric(airtable$`Speed(mph)`)
-  airtable$`Pace (min/mile)` <- ms(airtable$`Pace (min/mile)`)
-  airtable$`Pace (min/mile)` <- as.numeric(airtable$`Pace (min/mile)`)
-  airtable$`Elliptical Heart Rate (bpm)`<- as.integer(airtable$`Elliptical Heart Rate (bpm)`)
-  airtable$`Runkeeper Heart Rate (bpm)`<- as.integer(airtable$`Runkeeper Heart Rate (bpm)`)
-  airtable$`AppleWatch Heart Rate (bpm)`<- as.integer(airtable$`AppleWatch Heart Rate (bpm)`)
+  level <- as.integer(level)
+  time<- as.integer(time)
+  calories <- as.integer(calories)
+  miles <- as.numeric(miles)
+  distanceClimbed <- as.integer(distanceClimbed)
+  speed <- as.numeric(speed)
+  pace <- ms(pace)
+  pace <- as.numeric(pace)
+  ehr<- as.integer(ehr)
+  rhr<- as.integer(rhr)
+  awhr<- as.integer(awhr)
   
-  ## Line Graph of Various Heart Rates vs. Time
-  airtable <- airtable %>% mutate(Date = as.Date(Date))
-  airtable <- na.omit(airtable)
-  heart_rate <- c(airtable$`Elliptical Heart Rate (bpm)`, airtable$`Runkeeper Heart Rate (bpm)`, airtable$`AppleWatch Heart Rate (bpm)`)
-  plot(airtable$Date, heart_rate, type="l")
+  ## Calculate mean heart rate
+  airtable$'Mean Heart Rate (bpm)' <- (ehr + rhr + awhr)/3
+  mhr <- airtable$`Mean Heart Rate (bpm)`
   
-  #Bin Calories
-  calories <- airtable$Calories
+  ## Convert Date Column
+  airtable <- airtable %>% mutate(Date = as.Date(date))
+  date <- airtable$Date
+  
+  ## Bin Calories
   calorieLab <- c("100-200", "200-300", "300-400", "400-500", "500-600", "600-700", "700-800", "800-900")
   calorieBins <- cut(calories, pretty(calories), labels=calorieLab)
-  count(calorieBins)
-  plot(count(calorieBins), type="pie")
+  countCB <- count(calorieBins)
+  calorieBP <- ggplot(calorieBins, aes(x="", y=countCB, fill=calorieBins))+
+    geom_bar(width = 1, stat = "calorieBins")
+  calorieBP
   
-  #Bin Distance Climbed
-  distanceClimbed <- airtable$`Distance Climbed`
+  ## Bin Distance Climbed
   climbedLab <- c("2000-4000", "4000-6000", "6000-8000", "8000-10000")
   climbedBins <- cut(distanceClimbed, pretty(distanceClimbed), labels=climbedLab)
   count(climbedBins)
   plot(count(climbedBins), type="pie")
   
-  #Bin Speed
-  speed <- airtable$`Speed(mph)`
+  ## Bin Speed
   speedLab <- c("5-5.5", "5.5-6", "6-6.5", "6.5-7", "7-7.5")
   speedBins <- cut(speed, pretty(speed), labels=speedLab)
   count(speedBins)
   plot(count(speedBins), type="pie")
   
-  # Bin Pace
-  pace <- airtable$`Pace (min/mile)`
+  ## Bin Pace
   paceLab <- c("400-450", "450-500", "500-550", "550-600", "600-650", "650-700")
   paceBins <- cut(pace, pretty(pace), labels=paceLab)
   count(paceBins)
   plot(count(paceBins), type="pie")
   
+  ## Bin Mean Heart Rate
+  mhrLab <- c("120-125", "125-130", "130-135", "135-140", "140-145", "145-150", "150-155", "155-160")
+  mhrBins <- cut(mhr, pretty(mhr), labels=mhrLab)
+  count(mhrBins)
+  plot(count(mhrBins), type="p")
   
-  ## Calculate mean heart rate
-  airtable$'Mean Heart Rate (bpm)' <- (as.integer(airtable$`Elliptical Heart Rate (bpm)`) + as.integer(airtable$`Runkeeper Heart Rate (bpm)`) + as.integer(airtable$`AppleWatch Heart Rate (bpm)`))/3
-  airtable$`Mean Heart Rate (bpm)` <- as.integer(airtable$`Mean Heart Rate (bpm)`)
+  ## Bin Distance (Miles)
+  milesLab <- c("1-2", "3-4", "4-5", "5-6", "6-7")
+  milesBins <- cut(miles, pretty(miles))
+  count(milesBins)
+  plot(count(milesBins), type="p")
   
-  # Plot heart rate over time
-  x <- as.integer(c(airtable$`Elliptical Heart Rate (bpm)`, airtable$`Runkeeper Heart Rate (bpm)`, airtable$`AppleWatch Heart Rate (bpm)`))
-  plot(as.integer(airtable$`Pace (min/mile)`), as.integer(airtable$`Mean Heart Rate (bpm)`))
+  ## Plot Heart Rates over time and against mean
+  heart_rate <- c(ehr, rhr, awhr)
+  plot(date, ehr, type="l")
   
-  # Calculate instantaneous standard deviation.
-  airtable$'standard deviation' <- sd(x)
+  ## Plot heart rate against pace
+  plot(mhr, pace, type="p")
+  
+  ## Calculate instantaneous standard deviation.
+  airtable$'standard deviation' <- sd(heart_rate)
   airtable$`standard deviation`
   
-  # Boxplots
-  boxplot(airtable$`Mean Heart Rate (bpm)`~airtable$Level, xlab="Level", ylab="Mean Heart Rate (bpm)", main="Level")
-  boxplot(airtable$`Mean Heart Rate (bpm)`~airtable$`Time (Minutes)`, xlab="Time", ylab="Mean Heart Rate (bpm)", main="Time")
-  boxplot(airtable$`Mean Heart Rate (bpm)`~calorieBins, xlab="Calories", ylab="Mean Heart Rate (bpm)", main="Calories")
-  boxplot(airtable$`Mean Heart Rate (bpm)`~airtable$`Distance (Miles)`, xlab="Distance (Miles)", ylab="Mean Heart Rate (bpm)", main="Distance")
-  boxplot(airtable$`Mean Heart Rate (bpm)`~climbedBins, xlab="Distance Climbed", ylab="Mean Heart Rate (bpm)", main="Distance Climbed")
-  boxplot(airtable$`Mean Heart Rate (bpm)`~speedBins, xlab="Speed (mph)", ylab="Mean Heart Rate (bpm)", main="Speed")
-  boxplot(airtable$`Mean Heart Rate (bpm)`~paceBins, xlab="Pace (min/mile)", ylab="Mean Heart Rate (bpm)", main="Pace")
+  ## Boxplots
+  boxplot(mhr~level, xlab="Level", ylab="Mean Heart Rate (bpm)", main="Level")
+  boxplot(mhr~time, xlab="Time", ylab="Mean Heart Rate (bpm)", main="Time")
+  boxplot(mhr~calorieBins, xlab="Calories", ylab="Mean Heart Rate (bpm)", main="Calories")
+  boxplot(mhr~milesBins, xlab="Distance (Miles)", ylab="Mean Heart Rate (bpm)", main="Distance")
+  boxplot(mhr~climbedBins, xlab="Distance Climbed", ylab="Mean Heart Rate (bpm)", main="Distance Climbed")
+  boxplot(mhr~speedBins, xlab="Speed (mph)", ylab="Mean Heart Rate (bpm)", main="Speed")
+  boxplot(mhr~paceBins, xlab="Pace (min/mile)", ylab="Mean Heart Rate (bpm)", main="Pace")
   
-  # Pearson Correlation with Mean Heart Rate
-  cor(airtable$`Mean Heart Rate (bpm)`, airtable$Level, method = "pearson")
-  cor(airtable$`Mean Heart Rate (bpm)`, airtable$`Time (Minutes)`, method = "pearson")
-  cor(airtable$`Mean Heart Rate (bpm)`, calorieBins, method = "pearson")
-  cor(airtable$`Mean Heart Rate (bpm)`, airtable$`Distance (Miles)`, method = "pearson")
-  cor(airtable$`Mean Heart Rate (bpm)`, airtable$`Distance Climbed`, method = "pearson")
-  cor(airtable$`Mean Heart Rate (bpm)`, airtable$`Speed(mph)`, method = "pearson")
-  cor(airtable$`Mean Heart Rate (bpm)`, airtable$`Pace (min/mile)`, method = "pearson")
+  ## Pearson Correlation with Mean Heart Rate
+  cor(mhr, level, method = "pearson")
+  cor(mhr, time, method = "pearson")
+  cor(mhr, calories, method = "pearson")
+  cor(mhr, miles, method = "pearson")
+  cor(mhr, distanceClimbed, method = "pearson")
+  cor(mhr, speed, method = "pearson")
+  cor(mhr, pace, method = "pearson")
   
-  # ANOVA
-  anova - aov(airtable$'Mean Heart Rate (bpm)'~airtable$Level*airtable$`Time (Minutes)`*airtable$Calories*airtable$`Distance (Miles)`*airtable$`Distance Climbed`*airtable$`Speed(mph)`*airtable$`Pace (min/mile)`, data = airtable)
+  ## Scatter Plots
+  pairs(~ level + time + calories + speed + pace + ehr + rhr + awhr + mhr)
+  
+  ## ANOVA
+  anova <- aov(mhr ~ level * time * calories * miles * distanceClimbed * speed * pace)
   summary(anova)
